16c1e3b0e04aba7a2cfa298b31e072de2a3e7dfc
Chris-Coleongco <christien.coleongco@gmail.com>
2024-04-06 20:26:39 -0600

    whoops

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
       https://github.com/Chris-Coleongco/PushupCounterPytorch
       </a>
       <a href='https://github.com/Chris-Coleongco/ChurnPredictor'>
-      https://github.com/Chris-Coleongco/PushupCounterPytorch
+      https://github.com/Chris-Coleongco/ChurnPredictor
       </a>
     </>
   )
