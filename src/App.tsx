9c3d6391fd39616ae83d277e90ae1522f4f01a2e
Chris-Coleongco <christien.coleongco@gmail.com>
2024-08-06 21:51:13 -0600

    updated to actually cool stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+//       <code>src/App.tsx</code> and save to test HMR
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>XTN</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>``</h1>
       <p className="read-the-docs">
         just listing my cooler stuff here. this website is hella incomplete twt
       </p>
-      <a href='https://github.com/Chris-Coleongco/PushupCounterPytorch'>
-      https://github.com/Chris-Coleongco/PushupCounterPytorch
-      </a>
-      <a href='https://github.com/Chris-Coleongco/ChurnPredictor'>
-      https://github.com/Chris-Coleongco/ChurnPredictor
-      </a>
+      <br/>
+      <br/>
+      <a href='https://github.com/Chris-Coleongco/chatAppV2'>End to End Encrypted Chat app (in progress)</a>
+      <br/>
+      <br/>
+      <a href='https://github.com/Chris-Coleongco/stego-rust'>steganography (encoding secret messages in media), implemented in rust</a>
     </>
   )
 }
